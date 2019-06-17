7293659d24c1ec6de6a40406a75437cc154c53c6
Ivan <ivansertic7@gmail.com>
2019-06-17 03:35:19 +0200

    MainActivity -> RegisterActivity Adding image from device(not complete)

--- app/src/main/java/com/example/serticivan/letschat/RegisterActivity.kt
+++ app/src/main/java/com/example/serticivan/letschat/RegisterActivity.kt
@@ -1,14 +1,18 @@
 package com.example.serticivan.letschat
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +22,38 @@ class MainActivity : AppCompatActivity() {
             performRegister()
         }
 
+        selectphoto_button_register.setOnClickListener {
+            Log.d("RegisterActivity","try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent,0)
+        }
+
         already_have_account_text_view.setOnClickListener {
-            Log.d("MainActivity", "try to open login activity")
+            Log.d("RegisterActivity", "try to open login activity")
             //launch the login activity
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            //proceed and check what the selected image was
+            Log.d("RegisterActivity", "Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            selectphoto_button_register.setBackgroundDrawable(bitmapDrawable)
+        }
+
     }
 
     private fun performRegister() {
@@ -31,8 +61,8 @@ class MainActivity : AppCompatActivity() {
         val email = email_edittext_register.text.toString()
         val password = password_edittext_register.text.toString()
 
-        Log.d("MainActivity", "email is $email")
-        Log.d("MainActivity", "password: $password")
+        Log.d("RegisterActivity", "email is $email")
+        Log.d("RegisterActivity", "password: $password")
 
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
