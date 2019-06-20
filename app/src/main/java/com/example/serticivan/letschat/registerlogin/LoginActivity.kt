2f5cd5c14af5e1401ecb77a6e911cfaf99a4a102
Ivan <ivansertic7@gmail.com>
2019-06-20 12:55:05 +0200

    edited LoginActivity - added function to log in into app

--- app/src/main/java/com/example/serticivan/letschat/registerlogin/LoginActivity.kt
+++ app/src/main/java/com/example/serticivan/letschat/registerlogin/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.serticivan.letschat.registerlogin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.example.serticivan.letschat.R
+import com.example.serticivan.letschat.messages.LatestMessagesActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -15,18 +18,36 @@ class LoginActivity: AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_button_login.setOnClickListener {
-            val email = email_edittext_login.text.toString()
-            val password = password_edittext_login.text.toString()
-
-            Log.d("LoginActivity", "Attempt to login with email/pw: $email/**")
-
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            performLogin()
         }
 
-        back_to_registration_textview.setOnClickListener {
+        back_to_registration_textview.setOnClickListener{
             finish()
         }
     }
 
+    private fun performLogin() {
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email/pw.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Login", "Successfully logged in: ${it.result?.user?.uid}")
+
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
 
 }
